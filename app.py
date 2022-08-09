5cbabeb986b832c0560aafd3c26f551f8f822fed
bruno arch <bluisfernandes@gmail.com>
2022-08-09 18:42:53 -0300

    route users working

--- app.py
+++ app.py
@@ -1,65 +1,117 @@
 from flask import Flask, jsonify, request
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
-from tinydb import TinyDB, Query
+from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 from pydantic import BaseModel, Field
 from datetime import datetime
-from itertools import count
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+import os
+import secrets
+from dotenv import load_dotenv
+
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 
 from typing import Optional
+
+
+load_dotenv()
+
+# Make sure database URI is set
+if not os.environ.get('DATABASE_USER_URI'):
+    raise RuntimeError("DATABASE_USER_URI not set")
 
 app = Flask(__name__)
 spec = FlaskPydanticSpec('FlAsK', title='Estudo API')
 spec.register(app)
 
-c = count()
-database = TinyDB('database.json')
+app.secret_key = os.getenv('SECRET_KEY', secrets.token_urlsafe())
+ # Custom filter
 
-class QueryPessoa(BaseModel):
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_USER_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    group = db.Column(db.String(10), nullable=False, default='user')
+    email = db.Column(db.String(40), nullable=False)
+
+class Pesquisa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pesquisa = db.Column(db.String(40), nullable=False, unique=True)
+
+class Categoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    categoria = db.Column(db.String(20), nullable=False, unique=True)
+
+class Logs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.String(20), nullable=False)
+    job = db.Column(db.String(60), nullable=False)
+    area = db.Column(db.String(60), nullable=False)
+
+User_pydantic = sqlalchemy_to_pydantic(User)
+Pesquisa_pydantic = sqlalchemy_to_pydantic(Pesquisa)
+Categoria_pydantic = sqlalchemy_to_pydantic(Categoria)
+Logs_pydantic = sqlalchemy_to_pydantic(Logs)
+
+
+class QueryUser(BaseModel):
     id: Optional[int]
-    name: Optional[str]
-    age: Optional[int]
+    username: Optional[str]
+    group: Optional[str]
+    email: Optional[str]
 
-class Pessoa(BaseModel):
-    id: Optional[int] = Field(default_factory=lambda: next(c))
-    name: str
-    age: int
-
-class Pessoas(BaseModel):
-    pessoas: list[Pessoa]
+class Users(BaseModel):
+    users: list[User_pydantic]
     count: int
 
+
+migrate = Migrate(app, db)
 
 @app.get('/')
 def index():
     return {'homepage':'/', 'API version': 'v1'}
 
 
-@app.get('/pessoas')
-@spec.validate(query=QueryPessoa, resp=Response(HTTP_200=Pessoas))
-def busca_pessoas():
-    '''Retorna lista de pessoas'''
+@app.get('/users')
+@spec.validate(query=QueryUser, resp=Response(HTTP_200=Users))
+def find_users():
+    '''Return user list'''
     query = request.context.query
-    list_pessoas = database.search(Query().fragment(query.dict(exclude_none=True)))
+    users = User.query
+    for k, v in query.dict(exclude_none=False).items():
+        if v != None:
+            users = users.filter(getattr(User, k)==v)
+    list_users = users.all()
     return jsonify(
-            Pessoas(
-                pessoas=list_pessoas, 
-                count=len(list_pessoas)
+            Users(
+                users= list_users,
+                count= len(list_users)
             ).dict()
-    )
+    ) 
 
 @app.get('/pessoas/<int:id>')
-@spec.validate(resp=Response(HTTP_200=Pessoa))
+# @spec.validate(resp=Response(HTTP_200=Pessoa))
 def busca_pessoa(id):
     '''Retorna pessoa dado o id'''
     try:
         pessoa = database.search(Query().id == id)[0]
+        user = User.query.filter_by(username = session['username']).all()
     except IndexError:
         return {'message': 'Pessoa not found'}, 404
     return jsonify(pessoa)
 
 
 @app.post('/pessoas')
-@spec.validate(body=Request(Pessoa), resp=Response(HTTP_201=Pessoa))
+@spec.validate(body=Request(User_pydantic), resp=Response(HTTP_201=User_pydantic))
 def insere_pessoa():
     '''Insere uma pessoa no banco de dados'''
     person = request.context.body.dict()
@@ -68,7 +120,7 @@ def insere_pessoa():
 
 
 @app.put('/pessoas/<int:id>')
-@spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
+@spec.validate(body=Request(User_pydantic), resp=Response(HTTP_200=User_pydantic))
 def altera_pessoa(id):
     '''Altera a pessoa pelo numero do id.'''
     body = request.context.body.dict()
